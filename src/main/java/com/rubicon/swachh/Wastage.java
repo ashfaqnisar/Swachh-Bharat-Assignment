26f855aeaedc7da3a51cc638bcd683f5430cc825
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-08-05 20:27:40 +0530

    16. Created the wastage java file, which generates the wastage form.
    Signed-off-by: Ashfaq Nisar <ashfaqnisar00@gmail.com>

---
+++ src/main/java/com/rubicon/swachh/Wastage.java
@@ -0,0 +1,32 @@
+package com.rubicon.swachh;
+
+import com.rubicon.swachh.util.WastageType;
+
+import java.util.Scanner;
+
+public class Wastage {
+    private Scanner scanner = new Scanner(System.in);
+    private WastageType wastageType = new WastageType();
+
+    WastageType generateWastageForm() {
+        System.out.println("Type of Wastage: ");
+        System.out.println("Please, select the type of wastage. You would like to deposit in the " +
+                "Kiosk");
+        System.out.println("1. Glass");
+        System.out.println("2. Plastic");
+        System.out.println("3. Metal");
+
+        int switch_case = scanner.nextInt();
+        switch (switch_case) {
+            case 1:
+                wastageType.setGlass(true);
+            case 2:
+                wastageType.setPlastic(true);
+            case 3:
+                wastageType.setMetal(true);
+            default:
+                System.out.println("Please select the correct option");
+        }
+        return wastageType;
+    }
+}
