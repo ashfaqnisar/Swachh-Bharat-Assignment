2ade42b27c64ea6839d555a2365e77784a7af1f4
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-08-05 20:19:08 +0530

    15. Created the WastageType Data Class in the project
    Signed-off-by: Ashfaq Nisar <ashfaqnisar00@gmail.com>

---
+++ src/main/java/com/rubicon/swachh/util/WastageType.java
@@ -0,0 +1,40 @@
+package com.rubicon.swachh.util;
+
+public class WastageType {
+    private boolean glass = false;
+    private boolean metal = false;
+    private boolean plastic = false;
+
+    public WastageType() {
+    }
+
+    WastageType(boolean glass, boolean metal, boolean plastic) {
+        this.glass = glass;
+        this.metal = metal;
+        this.plastic = plastic;
+    }
+
+    public boolean isGlass() {
+        return glass;
+    }
+
+    public void setGlass(boolean glass) {
+        this.glass = glass;
+    }
+
+    public boolean isMetal() {
+        return metal;
+    }
+
+    public void setMetal(boolean metal) {
+        this.metal = metal;
+    }
+
+    public boolean isPlastic() {
+        return plastic;
+    }
+
+    public void setPlastic(boolean plastic) {
+        this.plastic = plastic;
+    }
+}
