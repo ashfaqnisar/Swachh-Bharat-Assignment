882f7b116f6fe6fc56c162072d218d2311133cb6
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-08-06 13:18:51 +0530

    32. Changed the name of the constants file

---
+++ src/main/java/com/rubicon/swachh/util/BrandsConstants.java
@@ -0,0 +1,11 @@
+package com.rubicon.swachh.util;
+
+import com.rubicon.swachh.models.BrandsType;
+
+public class BrandsConstants {
+    public static final BrandsType Sprite = new BrandsType("Sprite",45);
+    public static final BrandsType Coke = new BrandsType("Coke",32);
+    public static final BrandsType ThumbsUp = new BrandsType("Thumbsup",15);
+    public static final BrandsType Miranda = new BrandsType("Miranda",20);
+    public static final BrandsType Other = new BrandsType("Other",24);
+}
