6dd344e79cb39a96721e1f41df60dba9941ec52a
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-08-06 14:02:05 +0530

    34. Place the brand type in the App file
    Placed the brand thing in the App file to check whether the brand is working correctly or not.

--- src/main/java/com/rubicon/swachh/App.java
+++ src/main/java/com/rubicon/swachh/App.java
@@ -1,28 +1,29 @@
 package com.rubicon.swachh;
 
-import com.rubicon.swachh.models.Report;
-import com.thoughtworks.xstream.XStream;
+import com.rubicon.swachh.models.BrandType;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        Register register = new Register();
-        Wastage wastage = new Wastage();
-        Coupon coupon = new Coupon();
-
-        Report report = new Report();
-
-        XStream xstream = new XStream();
-
-        report.setUser(register.generateRegistrationForm());
-        report.setWastageType(wastage.generateWastageForm());
-        report.setCouponData(coupon.generateCouponCode());
-
-        String XML = xstream.toXML(report);
-
-        System.out.println(report.getReport());
-        System.out.println(XML);
+public class App {
+    public static void main(String[] args) {
+        Wastage wastage =new Wastage();
+        Brand brand = new Brand();
+        BrandType brandType = brand.generateBrandTypeForm(wastage.generateWastageForm());
+        System.out.println(brandType.toString());
+//        Register register = new Register();
+//        Wastage wastage = new Wastage();
+//        Coupon coupon = new Coupon();
+//
+//        Report report = new Report();
+//
+//        XStream xstream = new XStream();
+//
+//        report.setUser(register.generateRegistrationForm());
+//        report.setWastageType(wastage.generateWastageForm());
+//        report.setCouponData(coupon.generateCouponCode());
+//
+//        String XML = xstream.toXML(report);
+//
+//        System.out.println(report.getReport());
+//        System.out.println(XML);
 
 
 //        System.out.println(user2.getName());
