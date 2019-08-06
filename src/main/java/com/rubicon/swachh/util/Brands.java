0f117a347dd42c7b0abba8a8fa79fb937ab1a325
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-08-06 12:00:17 +0530

    31. The Brands.java is a constants file which has all the brands file

---
+++ src/main/java/com/rubicon/swachh/util/Brands.java
@@ -0,0 +1,11 @@
+package com.rubicon.swachh.util;
+
+import com.rubicon.swachh.models.BrandType;
+
+public class Brands {
+    public static final BrandType Sprite = new BrandType("Sprite",45);
+    public static final BrandType Coke = new BrandType("Sprite",45);
+    public static final BrandType ThumbsUp = new BrandType("Sprite",45);
+    public static final BrandType Other = new BrandType("Sprite",45);
+
+}
