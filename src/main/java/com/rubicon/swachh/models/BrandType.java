3dd76dde463f90fc6b8ac3d6a7df2332468c1424
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-08-06 14:46:54 +0530

    39. Modified the BrandType file to handle the inputs

--- src/main/java/com/rubicon/swachh/models/BrandType.java
+++ src/main/java/com/rubicon/swachh/models/BrandType.java
@@ -1,11 +1,70 @@
 package com.rubicon.swachh.models;
 
-import com.rubicon.swachh.util.Brands;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BrandType {
-    private String brandName;
-    private int brandCost;
-    private Brands brands = new Brands();
+    private boolean SpriteBrand = false;
+    private boolean CokeBrand = false;
+    private boolean DietCokeBrand = false;
+    private boolean ThumbsUpBrand = false;
+    private boolean OtherBrand = false;
+
+    private String brandName = "";
+    private int brandCost = 0;
+
+    private Map<String,Integer> brands= new HashMap<>();
+
+    public boolean isSpriteBrand() {
+        return SpriteBrand;
+    }
+
+    public void setSpriteBrand(boolean spriteBrand) {
+        SpriteBrand = spriteBrand;
+        setBrandName("Sprite");
+        setBrandCost(20);
+    }
+
+    public boolean isCokeBrand() {
+        return CokeBrand;
+    }
+
+    public void setCokeBrand(boolean cokeBrand) {
+        CokeBrand = cokeBrand;
+        setBrandName("Coke");
+        setBrandCost(10);
+    }
+
+    public boolean isDietCokeBrand() {
+        return DietCokeBrand;
+    }
+
+    public void setDietCokeBrand(boolean dietCokeBrand) {
+        DietCokeBrand = dietCokeBrand;
+        setBrandName("Diet Coke");
+        setBrandCost(25);
+    }
+
+    public boolean isThumbsUpBrand() {
+        return ThumbsUpBrand;
+    }
+
+    public void setThumbsUpBrand(boolean thumbsUpBrand) {
+        ThumbsUpBrand = thumbsUpBrand;
+        setBrandName("Thumbs Up");
+        setBrandCost(15);
+    }
+
+    public boolean isOtherBrand() {
+        return OtherBrand;
+    }
+
+    public void setOtherBrand(boolean otherBrand) {
+        OtherBrand = otherBrand;
+        setBrandName("Other");
+        setBrandCost(15);
+    }
 
     public BrandType() {
     }
@@ -21,12 +80,8 @@ public class BrandType {
 
     public void setBrandName(String brandName) {
         this.brandName = brandName;
-        checkForBrand();
     }
 
-    private void checkForBrand() {
-
-    }
 
     public int getBrandCost() {
         return brandCost;
@@ -34,6 +89,19 @@ public class BrandType {
 
     public void setBrandCost(int brandCost) {
         this.brandCost = brandCost;
+    }
+
+    @Override
+    public String toString() {
+        return "BrandType{" +
+                "SpriteBrand=" + SpriteBrand +
+                ", CokeBrand=" + CokeBrand +
+                ", DietCokeBrand=" + DietCokeBrand +
+                ", ThumbsUpBrand=" + ThumbsUpBrand +
+                ", OtherBrand=" + OtherBrand +
+                ", brandName='" + brandName + '\'' +
+                ", brandCost=" + brandCost +
+                '}';
     }
 
 }
