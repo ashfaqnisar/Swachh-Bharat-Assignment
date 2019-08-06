3c6fb3c80549ee361775ab2254bd76b5194866b1
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-08-06 14:19:46 +0530

    36. Added switch case statements in the brand file

--- src/main/java/com/rubicon/swachh/Brand.java
+++ src/main/java/com/rubicon/swachh/Brand.java
@@ -13,20 +13,48 @@ public class Brand {
     BrandType generateBrandTypeForm(WastageType wastageType){
         System.out.println("Brand: ");
         if (!wastageType.getWastageType().equals("Default")){
-            System.out.println("Please, provide the brand of the "+ wastageType.getWastageType() + "if known, else select the other option");
+            System.out.println("Please, provide the brand of the "+ wastageType.getWastageType() + " if known, else select the other option");
 
             System.out.println("\t1. Spirte");
             System.out.println("\t2. Coke");
-            System.out.println("\t3. Thumbs Up");
-            System.out.println("\t4. Other");
+            System.out.println("\t3. Diet Coke");
+            System.out.println("\t4. Thumbs Up");
+            System.out.println("\t5. Other");
 
             System.out.println("Enter the respective number for the brand: ");
+            int switch_case = scanner.nextInt();
 
-
+            switch (switch_case){
+                case 1:
+                    System.out.println("Sprite Brand has been selected.");
+                    brandType.setSpriteBrand(true);
+                    break;
+                case 2:
+                    System.out.println("Coke Brand has been selected.");
+                    brandType.setCokeBrand(true);
+                    break;
+                case 3:
+                    System.out.println("Diet Coke Brand has been selected.");
+                    brandType.setDietCokeBrand(true);
+                    break;
+                case 4:
+                    System.out.println("ThumbsUp Brand has been selected.");
+                    brandType.setThumbsUpBrand(true);
+                    break;
+                case 5:
+                    System.out.println("Other Brand has been selected.");
+                    brandType.setOtherBrand(true);
+                    break;
+                default:
+                    System.out.println("Please, select the correct option.");
+                    break;
+            }
         }
-
-
-
         return brandType;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{:"+ brandType.toString() ;
     }
 }
