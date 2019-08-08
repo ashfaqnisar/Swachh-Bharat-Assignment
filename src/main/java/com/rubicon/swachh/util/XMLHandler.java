72a4ba3755fbe42d7c642c800a6b8ad2d63c5188
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-08-08 13:43:38 +0530

    70. Created the XMLHandler File which handles the read & writes of XML file

--- src/main/java/com/rubicon/swachh/util/XMLHandler.java
+++ src/main/java/com/rubicon/swachh/util/XMLHandler.java
@@ -1,4 +1,57 @@
 package com.rubicon.swachh.util;
 
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import org.w3c.dom.Element;
+
+import java.io.FileWriter;
+import java.io.Writer;
+
 public class XMLHandler {
+
+    private static boolean isVoid(String str) {
+        return (str == null) || (str.trim().length() == 0);
+    }
+
+    private static boolean isVoid(Object obj) {
+        if (obj == null)
+            return true;
+        if ((obj instanceof String)) {
+            return ((String) obj).trim().length() == 0;
+        }
+        return false;
+    }
+    public static void write2File(Element element, String fileName) throws Exception {
+        if (!isVoid(fileName)) {
+            FileWriter sw = new FileWriter(fileName);
+            writeXml(element, sw, true);
+
+        }
+    }
+    private static boolean writeXml(Element element, Writer writer, boolean closeWriter) {
+
+        try {
+
+            if ((element != null) && (writer != null)) {
+                OutputFormat formatter = new OutputFormat();
+                formatter.setEncoding("UTF-8");
+                formatter.setIndenting(true);
+                XMLSerializer serializer = new XMLSerializer(writer, formatter);
+                serializer.serialize(element);
+                writer.flush();
+
+            }
+
+            if ((writer != null) && (closeWriter)) {
+                writer.close();
+
+            }
+            return true;
+
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+        return false;
+
+    }
 }
