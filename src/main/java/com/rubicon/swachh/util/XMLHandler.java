e9ccabdd4496aa5a9d92afb898a937bd99354b3e
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-08-08 14:05:00 +0530

    72. Successfully able to write the data into the XML with XMLHandler

--- src/main/java/com/rubicon/swachh/util/XMLHandler.java
+++ src/main/java/com/rubicon/swachh/util/XMLHandler.java
@@ -2,12 +2,21 @@ package com.rubicon.swachh.util;
 
 import com.sun.org.apache.xml.internal.serialize.OutputFormat;
 import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileWriter;
 import java.io.Writer;
+import java.util.ArrayList;
 
 public class XMLHandler {
+
+    private static ArrayList<DocumentBuilder> docBuilderList = new ArrayList();
+
 
     private static boolean isVoid(String str) {
         return (str == null) || (str.trim().length() == 0);
@@ -67,4 +76,48 @@ public class XMLHandler {
         return false;
 
     }
+    public static Document createDocument(String rootElementName) {//TODO: Need to construct same function but with different parameters
+        DocumentBuilder docBuild = null;
+        Document doc = null;
+
+        try {
+            docBuild = getDocumentBuilder();
+            doc = docBuild.newDocument();
+            Element root = doc.createElement(rootElementName);
+            doc.appendChild(root);
+            return doc;
+
+        } catch (ParserConfigurationException | FactoryConfigurationError e) {
+            System.out.print(e.getMessage());
+
+        } finally {
+            releaseDocumentBuilder(docBuild);
+
+        }
+        return doc;
+
+    }
+
+    private static synchronized void releaseDocumentBuilder(DocumentBuilder docBuild) {
+        if (docBuild != null) {
+            docBuilderList.add(docBuild);
+
+        }
+    }
+
+    public static synchronized DocumentBuilder getDocumentBuilder()
+            throws ParserConfigurationException, FactoryConfigurationError {
+        DocumentBuilder docBuilder = null;
+        if (docBuilderList.size() > 0) {
+            docBuilder = docBuilderList.remove(0);
+
+        }
+        if (docBuilder == null) {
+            docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+        }
+        return docBuilder;
+
+    }
+
 }
--- src/main/java/com/rubicon/swachh/util/XMLHandlerTest.java
+++ src/main/java/com/rubicon/swachh/util/XMLHandlerTest.java
@@ -1,4 +1,20 @@
 package com.rubicon.swachh.util;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class XMLHandlerTest {
+    public static void main(String[] args) throws Exception {
+        Document docUsers = XMLHandler.createDocument("Users");
+        Element eleUsers = docUsers.getDocumentElement();
+
+        Element eleUser = XMLHandler.createChild(eleUsers,"User");
+        Element eleUserName = XMLHandler.createChild(eleUser,"Name");
+        Element eleUserAge = XMLHandler.createChild(eleUser,"Age");
+
+        eleUserName.setTextContent("Ashfaq");
+        eleUserAge.setTextContent("21");
+        
+        XMLHandler.write2File(eleUsers,"Test.xml");
+    }
 }
