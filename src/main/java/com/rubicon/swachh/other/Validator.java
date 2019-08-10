1eefc56a3f3e8291b068a1598f0ca71c6ebbda1d
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-08-10 12:09:47 +0530

    86. Created the Validator & RegularExpConstants file

--- src/main/java/com/rubicon/swachh/other/Validator.java
+++ src/main/java/com/rubicon/swachh/other/Validator.java
@@ -1,4 +1,77 @@
 package com.rubicon.swachh.other;
 
+
+import java.util.Scanner;
+
+import static com.rubicon.swachh.util.RegularExpConstants.*;
+
 public class Validator {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public String checkName(String name) {
+        if (name.matches(NAME_REXP)) {
+            return name;
+        } else {
+            System.out.println("The First Character should not be a number.");
+            System.out.print("Enter Again: ");
+            name = checkName(scanner.next());
+            return name;
+        }
+    }
+
+    public String checkEmail(String email) {
+        if (email.matches(EMAIL_REXP)) {
+            return email;
+        } else {
+            System.out.println("\nPlease, Enter a valid Email:");
+            System.out.print("Enter Again: ");
+            email = checkEmail(scanner.next());
+            return email;
+        }
+    }
+
+    public Long checkNumber(String number) {
+        if (String.valueOf(number).matches(NUMBER_REXP)) {
+            return Long.parseLong(number);
+        } else {
+            System.out.println("\nPlease, Enter a valid phone number:");
+            System.out.print("Enter Again: ");
+
+            return (checkNumber(scanner.nextLine()));
+        }
+    }
+
+    public String checkAddress(String address) {
+        if (address.matches(ADDRESS_REXP)) {
+            return address;
+        } else {
+            System.out.println("\nPlease, Enter a valid address:");
+            System.out.print("Enter Again: ");
+
+            address = checkAddress(scanner.nextLine());
+            return address;
+        }
+
+    }
+
+    public int validateTypeInput(String typeSelected) {
+        if (typeSelected.matches(BRAND_REXP)) {
+            return Integer.parseInt(typeSelected);
+        } else {
+            System.out.println("\nPlease, Enter valid input for type:");
+            return validateTypeInput(scanner.next());
+        }
+    }
+
+    public int validateBrandInput(String brandSelected) {
+
+        if (brandSelected.matches(BRAND_REXP)) {
+            return Integer.parseInt(brandSelected);
+        } else {
+            System.out.println("\nPlease, Enter valid input:");
+
+            return validateBrandInput(scanner.next());
+        }
+    }
 }
---
+++ src/main/java/com/rubicon/swachh/util/RegularExpConstants.java
@@ -0,0 +1,13 @@
+package com.rubicon.swachh.util;
+
+public class RegularExpConstants {
+    public static final String NAME_REXP = "^[a-zA-Z][a-zA-Z0-9.,$;]+$";
+    public static final String EMAIL_REXP = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    public static final String NUMBER_REXP = "^[6-9]\\d{9}$";
+    public static final String ADDRESS_REXP="^[a-zA-Z0-9.,$;]{3,30}$";
+
+    public static final String BRAND_REXP="^[1-5$;]{1}$";
+    public static final String TYPE_REXP="^[1-3$;]{1}$";
+
+
+}
